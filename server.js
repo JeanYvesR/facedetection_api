c2557e88b99de28f868a8d760e1b4c4c6b37a760
JeanYvesR <JYRdev@outlook.com>
2021-11-30 07:47:09 -0300

    Adding database connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-acute-19235',
-    user : 'postgres',
-    password : 'roulet70T',
-    database : 'finalproject'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
