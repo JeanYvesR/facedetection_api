7ca2e8be1a03176d1d55764e74319d79d97e4c20
JeanYvesR <JYRdev@outlook.com>
2021-11-29 22:48:54 -0300

    Updating databse URL

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-acute-19235',
     user : 'postgres',
     password : 'roulet70T',
     database : 'finalproject'
